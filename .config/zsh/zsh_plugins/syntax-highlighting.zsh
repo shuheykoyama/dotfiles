5f22226c0588f6bc7eabf9c6fd737d0b6559b1d8
shuheykoyama <shuheykoyama@gmail.com>
2023-06-29 21:45:48 +0900

    feat: 💄 make the syntax highlight be cooler

---
+++ .config/zsh/zsh_plugins/syntax-highlighting.zsh
@@ -0,0 +1,26 @@
+# ~/zsh/zsh_plugins/syntax-highlighting.zsh
+
+# Configure the syntax highlight color:
+# command color:
+ZSH_HIGHLIGHT_STYLES[command]='fg='#268bd2''
+
+# alias color:
+ZSH_HIGHLIGHT_STYLES[alias]='fg='#268bd2''
+
+# shell builtin command color:
+ZSH_HIGHLIGHT_STYLES[builtin]='fg='#268bd2''
+
+# single-hyphen-option color:
+ZSH_HIGHLIGHT_STYLES[single-hyphen-option]='fg='#2aa198''
+
+# double-hyphen-option color:
+ZSH_HIGHLIGHT_STYLES[double-hyphen-option]='fg='#2aa198''
+
+# path color:
+# ZSH_HIGHLIGHT_STYLES[path]='fg='#2aa198''
+
+# default color:
+ZSH_HIGHLIGHT_STYLES[default]='fg='#2aa198''
+
+# the color of autosuggestion:
+ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg='#839496''
