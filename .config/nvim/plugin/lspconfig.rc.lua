135dcb29b189751bef8344e628829fec5a3995cf
shuheykoyama <shuheykoyama@gmail.com>
2023-07-29 07:38:38 +0900

    feat: :wrench: use a language server for JavaScript

--- .config/nvim/plugin/lspconfig.rc.lua
+++ .config/nvim/plugin/lspconfig.rc.lua
@@ -71,15 +71,14 @@ protocol.CompletionItemKind = {
 -- Set up completion using nvim_cmp with LSP source
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-nvim_lsp.flow.setup({
-	on_attach = on_attach,
-	capabilities = capabilities,
-})
+-- nvim_lsp.flow.setup({
+-- 	on_attach = on_attach,
+-- 	capabilities = capabilities,
+-- })
 
--- TypyScript
 nvim_lsp.tsserver.setup({
 	on_attach = on_attach,
-	filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
+	filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
 	cmd = { "typescript-language-server", "--stdio" },
 	capabilities = capabilities,
 })
