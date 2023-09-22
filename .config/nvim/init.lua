3d0d43bf1421e2e01e2c338335266f4463adde83
shuheykoyama <shuheykoyama@gmail.com>
2023-09-23 00:32:12 +0900

    feat: 🔧 add linux config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,11 +5,15 @@ require("plugins")
 
 local has = vim.fn.has
 local is_mac = has("macunix")
+local is_linux = has("unix")
 local is_win = has("win32")
 local is_wsl = has("wsl")
 
 if is_mac == 1 then
 	require("macos")
+end
+if is_linux == 1 then
+	require("linux")
 end
 if is_win == 1 then
 	require("windows")
