7f2f693ed62f796e6f260f7fea0ff62f5b0c3096
shuheykoyama <shuheykoyama@gmail.com>
2024-02-21 18:53:56 +0900

    feat: ✨ update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,23 +1,4 @@
-require("base")
-require("highlights")
-require("maps")
-require("plugins")
-
-local has = vim.fn.has
-local is_mac = has("macunix")
-local is_linux = has("unix")
-local is_win = has("win32")
-local is_wsl = has("wsl")
-
-if is_mac == 1 then
-	require("macos")
-end
-if is_linux == 1 then
-	require("linux")
-end
-if is_win == 1 then
-	require("windows")
-end
-if is_wsl == 1 then
-	require("wsl")
-end
+require("config.autocmds")
+require("config.keymaps")
+require("config.options")
+require("config.lazy")
