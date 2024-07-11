dd456de24dbe56a63867fb6c195696dab6dc46b0
shuheykoyama <shuheykoyama@gmail.com>
2024-07-12 00:53:43 +0900

    refactor: 💡 change dashboard message

--- .config/nvim/lua/plugins/alpha-nvim.lua
+++ .config/nvim/lua/plugins/alpha-nvim.lua
@@ -8,23 +8,27 @@ return {
 
     -- Set header
     dashboard.section.header.val = {
-      "                                                     ",
-      "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
-      "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
-      "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
-      "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
-      "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
-      "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
-      "                                                     ",
+      "                                                                                                            ",
+      "                                                                                                            ",
+      "                                                                                                            ",
+      "                                                                                                            ",
+      "                                                                                                            ",
+      " █████╗ ██████╗ ███████╗    ██╗   ██╗ ██████╗ ██╗   ██╗    ██████╗ ███████╗ █████╗ ██████╗ ██╗   ██╗██████╗ ",
+      "██╔══██╗██╔══██╗██╔════╝    ╚██╗ ██╔╝██╔═══██╗██║   ██║    ██╔══██╗██╔════╝██╔══██╗██╔══██╗╚██╗ ██╔╝╚════██╗",
+      "███████║██████╔╝█████╗       ╚████╔╝ ██║   ██║██║   ██║    ██████╔╝█████╗  ███████║██║  ██║ ╚████╔╝   ▄███╔╝",
+      "██╔══██║██╔══██╗██╔══╝        ╚██╔╝  ██║   ██║██║   ██║    ██╔══██╗██╔══╝  ██╔══██║██║  ██║  ╚██╔╝    ▀▀══╝ ",
+      "██║  ██║██║  ██║███████╗       ██║   ╚██████╔╝╚██████╔╝    ██║  ██║███████╗██║  ██║██████╔╝   ██║     ██╗   ",
+      "╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝       ╚═╝    ╚═════╝  ╚═════╝     ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚═════╝    ╚═╝     ╚═╝   ",
+      "                                                                                                            ",
     }
 
     -- Set menu
     dashboard.section.buttons.val = {
       dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
-      dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
-      dashboard.button("SPC ff", "󰱼  > Find File", "<cmd>Telescope find_files<CR>"),
-      dashboard.button("SPC fs", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
-      dashboard.button("SPC wr", "󰁯  > Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
+      dashboard.button("SPC e", "  > Toggle file explorer", "<cmd>Neotree<CR>"),
+      dashboard.button(";f", "󰱼  > Find File", "<cmd>Telescope find_files<CR>"),
+      dashboard.button(";r", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
+      -- dashboard.button("SPC wr", "󰁯  > Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
       dashboard.button("q", "  > Quit NVIM", "<cmd>qa<CR>"),
     }
 
