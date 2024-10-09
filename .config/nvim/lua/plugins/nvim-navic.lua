266c0af3f78f21a0f4a85da69aef399e5d9d74d8
shuheykoyama <shuheykoyama@gmail.com>
2024-10-09 09:02:57 +0900

    feat: 🎸 add nvim-navic.lua

---
+++ .config/nvim/lua/plugins/nvim-navic.lua
@@ -0,0 +1,25 @@
+return {
+  "SmiteshP/nvim-navic",
+  lazy = true,
+  init = function()
+    vim.g.navic_silence = true
+  end,
+  opts = function()
+    return {
+      lsp = {
+        auto_attach = true,
+        preference = nil,
+      },
+      highlight = true,
+      separator = "  ",
+      depth_limit = 3,
+      depth_limit_indicator = "..",
+      safe_output = true,
+      lazy_update_context = false,
+      click = false,
+      format_text = function(text)
+        return text
+      end,
+    }
+  end,
+}
