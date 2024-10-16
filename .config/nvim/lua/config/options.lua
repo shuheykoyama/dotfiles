53344ff3b16d9e8c7e068dba21d843ff3d37dc13
shuheykoyama <shuheykoyama@gmail.com>
2024-10-16 23:33:00 +0900

    feat: add a feature to keep undo history

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,30 +1,59 @@
+-- edit
 vim.cmd("language en_US.utf-8")
-
-local opt = vim.opt -- for conciseness
-
 vim.scriptencoding = "utf-8"
-opt.encoding = "utf-8"
-opt.fileencoding = "utf-8"
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
+vim.opt.wrap = false
+vim.g.mapleader = " "
 
--- enable mouse support
-opt.mouse = "a"
+-- search
+vim.opt.ignorecase = true -- Case insensitive searching UNLESS /C or capital in search
+vim.opt.smartcase = true
+vim.opt.incsearch = true
+vim.opt.hlsearch = true
 
--- Settings from craftzdog's dotfiles
-opt.title = true
-opt.hlsearch = true
-opt.backup = false
-opt.showcmd = true
-opt.cmdheight = 1
+-- clipboard
+vim.opt.clipboard:append("unnamedplus")
+
+-- indent
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.smarttab = true
+vim.opt.breakindent = true
+
+-- split
+vim.opt.splitbelow = true -- Put new windows below current
+vim.opt.splitright = true -- Put new windows right of current
+vim.opt.splitkeep = "cursor"
+
+-- scroll
+vim.opt.scrolloff = 10
+
+-- disable netrw
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- appearance
+vim.opt.termguicolors = true
+vim.opt.title = true
+vim.opt.cursorline = true
+vim.opt.mouse = ""
+
+-- sign
+vim.opt.signcolumn = "yes"
+
+-- cmdheight
+vim.opt.cmdheight = 1
 if vim.fn.has("nvim-0.8") == 1 then
   vim.opt.cmdheight = 0
 end
-opt.laststatus = 2
-opt.scrolloff = 10
-opt.shell = "zsh"
-opt.backupskip = { "/tmp/*", "/private/tmp/*" }
-opt.inccommand = "split"
-opt.wildignore:append({ "*/node_modules/*" })
-opt.splitkeep = "cursor"
+
+-- line number
+vim.opt.relativenumber = true
+vim.opt.number = true
 
 -- Undercurl
 vim.cmd([[let &t_Cs = "\e[4:3m"]])
@@ -33,57 +62,22 @@ vim.cmd([[let &t_Ce = "\e[4:0m"]])
 -- Add asterisks in block comments
 vim.opt.formatoptions:append({ "r" })
 
-vim.cmd([[au BufNewFile,BufRead *.astro setf astro]])
-vim.cmd([[au BufNewFile,BufRead Podfile setf ruby]])
-
--- The above is his setting
-
--- disable Netrw
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
 -- skip loading deprecated nvim-treesitter module to avoid startup warning message
 vim.g.skip_ts_context_commentstring_module = true
 
--- line numbers
-opt.relativenumber = true -- show relative line numbers
-opt.number = true -- shows absolute line number on cursor line (when relative number is on)
+-- automatically saves undo history
+vim.opt.undofile = true
 
--- tabs & indentation
-opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
-opt.shiftwidth = 2 -- 2 spaces for indent width
-opt.expandtab = true -- expand tab to spaces
-opt.autoindent = true -- copy indent from current line when starting new one
-opt.smartindent = true
-opt.breakindent = true
-opt.smarttab = true
+-- others
+vim.opt.backup = false
+vim.opt.showcmd = true
+vim.opt.laststatus = 3
+vim.opt.shell = "zsh"
+vim.opt.backupskip = { "/tmp/*", "/private/tmp/*" }
+vim.opt.inccommand = "split"
+vim.opt.backspace = { "start", "eol", "indent" }
+vim.opt.path:append({ "**" }) -- Finding files - Search down into subfolders
+vim.opt.wildignore:append({ "*/node_modules/*" })
 
--- line wrapping
-opt.wrap = false -- disable line wrapping
-
--- search settings
-opt.ignorecase = true -- Case insensitive searching UNLESS /C or capital in search
-opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
-opt.path:append({ "**" }) -- Finding files - Search down into subfolders
-
--- cursor line
-opt.cursorline = true -- highlight the current cursor line
-
--- appearance
-
--- turn on termguicolors for nightfly colorscheme to work
--- (have to use iterm2 or any other true color terminal)
-opt.termguicolors = true
-opt.background = "dark" -- colorschemes that can be light or dark will be made dark
-opt.signcolumn = "yes" -- show sign column so that text doesn't shift
-
--- backspace
--- allow backspace on indent, end of line or insert mode start position
-opt.backspace = { "start", "eol", "indent" }
-
--- clipboard
-opt.clipboard:append("unnamedplus") -- use system clipboard as default register
-
--- split windows
-opt.splitright = true -- split vertical window to the right
-opt.splitbelow = true -- split horizontal window to the bottom
+vim.cmd([[au BufNewFile,BufRead *.astro setf astro]])
+vim.cmd([[au BufNewFile,BufRead Podfile setf ruby]])
