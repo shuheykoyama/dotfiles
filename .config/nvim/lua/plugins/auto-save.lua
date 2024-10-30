aa36951b317aba6db3c476a98e8a189938c21ecf
shuheykoyama <shuheykoyama@gmail.com>
2024-10-30 20:50:37 +0900

    feat(nvim): add timestamp message to auto-save plugin configuration

--- .config/nvim/lua/plugins/auto-save.lua
+++ .config/nvim/lua/plugins/auto-save.lua
@@ -2,6 +2,13 @@ return {
   "Pocco81/auto-save.nvim",
   event = { "BufReadPre", "BufNewFile" },
   config = function()
-    require("auto-save").setup({})
+    require("auto-save").setup({
+      execution_message = {
+        message = function() -- message to print on save
+          return ("AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S"))
+        end,
+        dim = 0,
+      },
+    })
   end,
 }
