c4698ee42209fc9f3447d56ff72f78936dbaf3f0
shuheykoyama <shuheykoyama@gmail.com>
2024-10-30 20:49:39 +0900

    refactor(nvim): remove auto-save feature on exiting insert mode

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -12,15 +12,3 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt.conceallevel = 0
   end,
 })
-
--- Auto-save when leaving insert mode
-vim.api.nvim_create_autocmd("InsertLeave", {
-  pattern = "*",
-  callback = function()
-    if vim.bo.modified then
-      vim.cmd("silent! write")
-      vim.notify("saved at " .. vim.fn.strftime("%H:%M:%S"), "", { title = "AutoSave:" })
-    end
-  end,
-  nested = true,
-})
