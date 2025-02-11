0639ec09f6909bd02899cc4b602d5d4729bbd353
shuheykoyama <shuheykoyama@gmail.com>
2025-02-11 23:02:16 +0900

    fix: :bug: switch to solarized-osaka colors and adjust highlight groups

--- .config/nvim/lua/plugins/incline.lua
+++ .config/nvim/lua/plugins/incline.lua
@@ -2,19 +2,19 @@ return {
   "b0o/incline.nvim",
   event = { "FocusLost", "CursorHold" },
   config = function()
-    local colors = require("tokyonight.colors.night")
-    -- local colors = require("solarized-osaka.colors").setup()
+    -- local colors = require("tokyonight.colors.night")
+    local colors = require("solarized-osaka.colors").setup()
     -- local colors = require("github-theme.palette").load("github_dark")
     require("incline").setup({
       highlight = {
-        groups = {
-          InclineNormal = { guibg = colors.magenta2, guifg = "#000000" },
-          InclineNormalNC = { guibg = colors.terminal_black, guifg = "#000000" },
-        },
         -- groups = {
-        --   InclineNormal = { guibg = colors.magenta500, guifg = colors.base04 },
-        --   InclineNormalNC = { guifg = colors.violet500, guibg = colors.base03 },
+        --   InclineNormal = { guibg = colors.magenta2, guifg = "#000000" },
+        --   InclineNormalNC = { guibg = colors.terminal_black, guifg = "#000000" },
         -- },
+        groups = {
+          InclineNormal = { guibg = colors.magenta500, guifg = colors.base04 },
+          InclineNormalNC = { guifg = colors.violet500, guibg = colors.base03 },
+        },
       },
       window = { margin = { vertical = 0, horizontal = 1 } },
       hide = {
